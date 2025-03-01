e69035bc8777bbf0921a679b15c54b19dc57401d
trapido02 <trapido02.2@gmail.com>
2025-03-01 23:58:45 +0100

    First commit

---
+++ source/main.cpp
@@ -0,0 +1,34 @@
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+
+#include <cstddef>
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello, world!" << std::endl;
+
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
